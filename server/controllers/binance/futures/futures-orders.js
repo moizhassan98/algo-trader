ff11a350a92f897c4615e116cfde468e49b3787d
Moiz Hassan <66639544+moizhassan98@users.noreply.github.com>
2023-01-24 21:46:02 +0500

    added validation to the future orders apis.

--- server/controllers/binance/futures/futures-orders.js
+++ server/controllers/binance/futures/futures-orders.js
@@ -1,4 +1,11 @@
 const { binance } = require('../helper/binance-api')
+const {
+    createFuturesOrderSchema,
+    getFuturesOrderStatusSchema,
+    cancelFuturesOrderSchema,
+    cancelAllFuturesOrdersSchema,
+    getAllFuturesOrdersSchema,
+} = require('./utils/validators')
 
 
 /**
@@ -11,57 +18,81 @@ const createFuturesOrder = async(options) =>{
     const apiKey = process.env.FuturesTestnetApiKey
     const apiSecret = process.env.FuturesTestnetApiSecret
 
-    // console.log(apiKey,apiSecret);
+    const {error} = createFuturesOrderSchema.validate(options);
 
-    var result = await binance(
-        '/fapi/v1/order',
-        'POST',
-        apiKey,
-        apiSecret,
-        options,
-    );
-    if(result.status === 200 ){
+    if(error){
         return ({
-            status: 200,
-            success: true,
-            result: result.data
-        })
-    }else{
-        return ({
-            status: result.response.status,
+            status: 400,
             success: false,
-            error: result.response.statusText,
-            data: result.response.data ? result.response.data : "no data",
+            error: error
         })
     }
+    else{
+        var result = await binance(
+            '/fapi/v1/order',
+            'POST',
+            apiKey,
+            apiSecret,
+            options,
+        );
+        if(result.status === 200 ){
+            return ({
+                status: 200,
+                success: true,
+                result: result.data
+            })
+        }
+        else{
+            return ({
+                status: result.response.status,
+                success: false,
+                error: result.response.statusText,
+                data: result.response.data ? result.response.data : "no data",
+            })
+        }
+    }
+
 }
 
 const getFuturesOrderStatus = async(options) =>{
     const apiKey = process.env.FuturesTestnetApiKey
     const apiSecret = process.env.FuturesTestnetApiSecret
 
-    var result = await binance(
-        '/fapi/v1/order',
-        'GET',
-        apiKey,
-        apiSecret,
-        options
-    );
+    const {error} = getFuturesOrderStatusSchema.validate(options);
 
-    if(result.status === 200 ){
+    if(error){
         return ({
-            status: 200,
-            success: true,
-            result: result.data
-        })
-    }else{
-        return ({
-            status: result.response.status,
+            status: 400,
             success: false,
-            error: result.response.statusText,
-            data: result.response.data ? result.response.data : "no data",
+            error: error
         })
     }
+    else{
+        var result = await binance(
+            '/fapi/v1/order',
+            'GET',
+            apiKey,
+            apiSecret,
+            options
+        );
+    
+        if(result.status === 200 ){
+            return ({
+                status: 200,
+                success: true,
+                result: result.data
+            })
+        }
+        else{
+            return ({
+                status: result.response.status,
+                success: false,
+                error: result.response.statusText,
+                data: result.response.data ? result.response.data : "no data",
+            })
+        }
+    }
+    
 
 }
 
@@ -69,84 +100,124 @@ const cancelFuturesOrder = async(options) => { //it only cancels the non fulfill
     const apiKey = process.env.FuturesTestnetApiKey
     const apiSecret = process.env.FuturesTestnetApiSecret
 
-    var result = await binance(
-        '/fapi/v1/order',
-        'DELETE',
-        apiKey,
-        apiSecret,
-        options
-    );
+    const {error} = cancelFuturesOrderSchema.validate(options);
 
-    if(result.status === 200 ){
+    if(error){
         return ({
-            status: 200,
-            success: true,
-            result: result.data
-        })
-    }else{
-        return ({
-            status: result.response.status,
-            success: true,
-            error: result.response.statusText,
-            data: result.response.data ? result.response.data : "no data",
+            status: 400,
+            success: false,
+            error: error
         })
     }
+    else{
+        var result = await binance(
+            '/fapi/v1/order',
+            'DELETE',
+            apiKey,
+            apiSecret,
+            options
+        );
+    
+        if(result.status === 200 ){
+            return ({
+                status: 200,
+                success: true,
+                result: result.data
+            })
+        }
+        else{
+            return ({
+                status: result.response.status,
+                success: true,
+                error: result.response.statusText,
+                data: result.response.data ? result.response.data : "no data",
+            })
+        }
+    }
+
+    
 }
 
 const cancelAllFuturesOrders = async(options) =>{// Cancel all unfullfilled orders
     const apiKey = process.env.FuturesTestnetApiKey
     const apiSecret = process.env.FuturesTestnetApiSecret
 
-    var result = await binance(
-        '/fapi/v1/allOpenOrders',
-        'DELETE',
-        apiKey,
-        apiSecret,
-        options
-    );
+    const {error} = cancelAllFuturesOrdersSchema.validate(options);
 
-    if(result.status === 200 ){
+    if(error){
         return ({
-            status: 200,
-            success: true,
-            result: result.data
-        })
-    }else{
-        return ({
-            status: result.response.status,
-            succcess: false,
-            error: result.response.statusText,
-            data: result.response.data ? result.response.data : "no data",
+            status: 400,
+            success: false,
+            error: error
         })
     }
+    else{
+        var result = await binance(
+            '/fapi/v1/allOpenOrders',
+            'DELETE',
+            apiKey,
+            apiSecret,
+            options
+        );
+    
+        if(result.status === 200 ){
+            return ({
+                status: 200,
+                success: true,
+                result: result.data
+            })
+        }
+        else{
+            return ({
+                status: result.response.status,
+                succcess: false,
+                error: result.response.statusText,
+                data: result.response.data ? result.response.data : "no data",
+            })
+        }
+    }
+    
 }
 
 const getAllFuturesOrders = async(options) =>{
     const apiKey = process.env.FuturesTestnetApiKey
     const apiSecret = process.env.FuturesTestnetApiSecret
 
-    var result = await binance(
-        '/fapi/v1/allOrders',
-        'GET',
-        apiKey,
-        apiSecret,
-        options
-    );
+    const {error} = getAllFuturesOrdersSchema.validate(options);
 
-    if(result.status === 200 ){
+    if(error){
         return ({
-            status: 200,
-            success: true,
-            result: result.data
-        })
-    }else{
-        return ({
-            status: result.response.status,
+            status: 400,
             success: false,
-            error: result.response.statusText,
-            data: result.response.data ? result.response.data : "no data",
+            error: error
         })
     }
+    else{
+        var result = await binance(
+            '/fapi/v1/allOrders',
+            'GET',
+            apiKey,
+            apiSecret,
+            options
+        );
+    
+        if(result.status === 200 ){
+            return ({
+                status: 200,
+                success: true,
+                result: result.data
+            })
+        }
+        else{
+            return ({
+                status: result.response.status,
+                success: false,
+                error: result.response.statusText,
+                data: result.response.data ? result.response.data : "no data",
+            })
+        }
+    }
+    
 }
 
 module.exports = {
