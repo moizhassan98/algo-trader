84c887bb6726056e32516f5ebdd052166fd3691f
Moiz Hassan <66639544+moizhassan98@users.noreply.github.com>
2023-02-18 21:46:27 +0500

    init react starter

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import {
+    Col,
+    Row,
+    Container
+} from 'reactstrap'
+
+
+const Home = () =>{
+    return (
+        <Container>
+            Hello
+        </Container>
+    )
+}
+
+export default Home
